3e6ddd206adb1154f131e6d1780c7c25f64c879c
Changela, Kevin <kevin.changela@sap.com>
2018-05-23 21:46:51 -0500

    Updated SSO logic if no Welcome Message is sent

--- src/actions/channel.js
+++ src/actions/channel.js
@@ -1,19 +1,14 @@
 import config from '../config'
-
-/*** i820703 ***/
-import { updateWelcomeMessage } from 'sso'
-/*** i820703 ***/
+import { updateWelcomeMessage } from 'sso' // i820703
 
 export const getChannelPreferences = (channelId, token) =>
-  fetch(`${config.apiUrl}/webhook/${channelId}/preferences`, {
-    method: 'get',
-    headers: {
-      Authorization: token,
-      Accept: 'application/json',
-    },
-  })
+    fetch(`${config.apiUrl}/webhook/${channelId}/preferences`, {
+        method: 'get',
+        headers: {
+            Authorization: token,
+            Accept: 'application/json',
+        },
+    })
     .then(res => res.json())
     .then(data => data.results)
-/*** i820703 ***/    
-    .then(data => updateWelcomeMessage(data))
-/*** i820703 ***/    
+    .then(data => updateWelcomeMessage(data)) // i820703
--- src/sso.js
+++ src/sso.js
@@ -1,13 +1,16 @@
-/*** i820703 ***/
 export const updateWelcomeMessage = (data) => {
-    return getCurrentUserInfo()
-        .then(function(user) {
-            data.welcomeMessage = data.welcomeMessage
-                .replace("<first>", user.firstName)
-                .replace("<last>", user.lastName)
-                .replace("<id>", user.name)
-            return data
-        })
+    if (data.welcomeMessage) {
+        return getCurrentUserInfo()
+            .then(function(user) {
+                data.welcomeMessage = data.welcomeMessage
+                    .replace("<first>", user.firstName)
+                    .replace("<last>", user.lastName)
+                    .replace("<id>", user.name)
+                return data
+            })
+    } else {
+        return data
+    }
 }
 
 const getCurrentUserInfo = () => {
@@ -22,4 +25,3 @@ const getCurrentUserInfo = () => {
             return data;
         })
 }
-/*** i820703 ***/
